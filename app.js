180486ae1fe5ac0e34efb0c41e6fdd7444bd30d5
Anirudh Santhanam <144428771+Anyr00d@users.noreply.github.com>
2024-03-16 12:33:28 +0530

    add wallet detection
    add wallet detection to avoid duplicate casting of votes

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let yes_votes=0;
 let no_votes=0;
 
-let a={poll1:{name: "CAA Implementation", desc: "Citizenship Amendment Act", options: {option1:["yes",yes_votes], option2: ["no",no_votes], option3:null,option4:null,option5:null}}}
-
+let a={poll1:{name: "CAA Implementation", desc: "Citizenship Amendment Act", options: {option1:["yes",yes_votes], option2: ["no",no_votes], option3:null,option4:null,option5:null},whoVoted:[]}}
+let whoVotedArray=a.poll1.whoVoted;
 
 app.get("/", (req, res) => {
   res.render("homepage.ejs")
@@ -21,7 +21,16 @@ app.get("/", (req, res) => {
 
 app.post('/account',(req,res)=>{
   const username= req.body.name;
+  const walletId=req.body.wallet;
   console.log(username);
+  console.log(walletId);
+  whoVotedArray.push(walletId);
+  console.log(a);
+  for(let i=0;i<whoVotedArray.length;i++){
+    if(whoVotedArray[i]==walletId){
+      res.send("You can't vote");
+    }
+  }
   res.render("index.ejs")
 })
 
@@ -36,7 +45,7 @@ app.post("/voting",(req,res)=>{
     options_.option1[1]++;
     console.log("after:" + options_.option1[1]);
   }
-  else{
+  else if(vote==options_.option2[0]){
     console.log("before:" + options_.option2[1])
     options_.option2[1]++;
     console.log("after:" + options_.option2[1])
